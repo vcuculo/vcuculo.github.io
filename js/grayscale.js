08750c938a589b6f432dee44d2f16cc8566334a2
vcuculo <lasek88@gmail.com>
2017-10-10 01:11:41 +0200

    upgrade to mood site

--- js/grayscale.js
+++ js/grayscale.js
@@ -16,15 +16,34 @@ $(window).scroll(function() {
 
 });
 
+$('#math').on('touchstart mouseenter', function () {
+      $('#math').text('in Affective Computing');
+    }
+);
+
+$('#math').on('touchend mouseleave', function () {
+      $('#math').text('in Mathematical Sciences');
+    }
+);
+
 $('#surname').on('touchstart mouseenter', function () {
       $('#cuckoo').stop().animate({"left": "0px"}, 2000);
+      $('#surname').text('Cuckoo');
     }
 );
 
 $('#surname').on('touchend mouseleave', function () {
       $('#cuckoo').stop().animate({"left": "-200px"}, 2000);
+      $('#surname').text('Cuculo');
     }
 );
+
+$(function() {
+    $('.light-bulb').bind('click', function(event) {
+        turnOnLight();
+    });
+});
+
 
 $(function() {
     $('.page-scroll a').bind('click', function(event) {
@@ -93,9 +112,118 @@ $(document).ready(function() {
     .animate('#dot',{ delay: $(window).height()/2, duration: $(window).height()/2, property:'left', start:-$(window).width(), end:0 })
     .animate('#unimi',{ delay: $(window).height()/2, duration: $(window).height()/2, property:'right', start:-$(window).width(), end:0 })
     .animate('#portfoliolist',{delay: 100, duration: $(window).height()-100, property:'opacity', start:0 });
+
+    $('#intro-modal').modal('show');
+
 });
 
 window.onorientationchange = function()
 {
    window.location.reload();
 };
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+function gumSuccess( stream ) {
+    // add camera stream if getUserMedia succeeded
+    if ("srcObject" in vid) {
+        vid.srcObject = stream;
+    } else {
+        vid.src = (window.URL && window.URL.createObjectURL(stream));
+    }
+    vid.onloadedmetadata = function() {
+        adjustVideoProportions();
+        vid.play();
+    }
+    vid.onresize = function() {
+        adjustVideoProportions();
+        if (trackingStarted) {
+            ctrack.stop();
+            ctrack.reset();
+            ctrack.start(vid);
+        }
+    }
+    startVideo();
+}
+
+function gumFail() {
+    alert("There was some problem trying to get video from your webcam. To continue the navigation just click on the small bulb on the top right.");
+}
+
+
+var vid = document.getElementById('videoel');
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.URL = window.URL || window.webkitURL || window.msURL || window.mozURL;
+
+// check for camerasupport
+if (navigator.mediaDevices) {
+    navigator.mediaDevices.getUserMedia({video : true}).then(gumSuccess).catch(gumFail);
+} else if (navigator.getUserMedia) {
+    navigator.getUserMedia({video : true}, gumSuccess, gumFail);
+} else {
+    alert("It seems like you don't have a webcam or your browser does not support this feature. To continue the navigation just click on the small bulb on the top right.");
+}
+
+/*********** setup of emotion detection *************/
+
+// set eigenvector 9 and 11 to not be regularized. This is to better detect motion of the eyebrows
+pModel.shapeModel.nonRegularizedVectors.push(9);
+pModel.shapeModel.nonRegularizedVectors.push(11);
+
+var ctrack = new clm.tracker({useWebGL : true});
+ctrack.init(pModel);
+var trackingStarted = false;
+
+var light = false;
+
+function turnOnLight() {
+  if (!light){
+    ctrack.stop();
+    $('#fadeMe').animate({opacity: 0, filter: 'alpha(opacity=0)'}); // IE fallback
+    $('.fa-lightbulb-o').css('color','yellow');
+    $('.fa-lightbulb-o').removeClass('fa-spin');
+    light = true;
+  }
+}
+
+
+function startVideo() {
+    // start video
+    vid.play();
+    // start tracking
+    ctrack.start(vid);
+    trackingStarted = true;
+    // start loop to draw face
+    drawLoop();
+}
+
+function drawLoop() {
+    requestAnimFrame(drawLoop);
+
+    var cp = ctrack.getCurrentParameters();
+
+    var er = ec.meanPredict(cp);
+    if (er && !light) {
+        var value = 1 - er[0].value;
+        var element = document.getElementById('fadeMe');
+
+        if (ctrack.getScore() > 0.5) {
+          $('#noface').hide();          
+          $('#fadeMe').css('opacity', value, 'filter', 'alpha(opacity=' + value + ')');
+        } else {
+          $('#noface').show();
+          $('#fadeMe').css('opacity', '1', 'filter', 'alpha(opacity=1)');
+        }
+    }
+}
+delete emotionModel['angry'];
+delete emotionModel['disgusted'];
+delete emotionModel['fear'];
+delete emotionModel['surprised'];
+delete emotionModel['sad'];
+
+var ec = new emotionClassifier();
+ec.init(emotionModel);
